9fe09a5c8801eedef853c8493ddd483a35589933
Wojciech Piekos <wojciech.piekos@gmail.com>
2024-01-25 19:00:51 +0100

    add category page functionality

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -33,4 +33,38 @@ export default async function handle(req,res) {
             res.status(500).json(error.message)
         }
     }
+
+    if (method === 'PUT') {
+        const { name, parentCategory, _id } = req.body
+        if (!name, !_id) {
+            res.status(400).json({ message: false })
+        }
+        try {
+            const response = await Category.findByIdAndUpdate(_id, {_id, name, parent: parentCategory}, {new: true})
+            res.status(201).json(response)
+        } catch (error) {
+            console.log(error.message)
+            res.status(500).json(error.message)
+        }
+    }
+
+    if (method === 'DELETE') {
+        const { id } = req.query
+
+
+        if (!id) {
+            res.status(400).json({ message: false })
+        }
+
+        try {
+            const deletedCategory = await Category.findByIdAndDelete(id).exec()
+            if (!deletedCategory) {
+                res.status(400).json({message: false})
+            }
+            res.status(200).json({ message: true })
+        } catch (error) {
+            console.log(error.message)
+            res.status(400).json({ message: false })
+        }
+    }
 }
--- pages/categories.jsx
+++ pages/categories.jsx
@@ -1,8 +1,11 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { withSwal } from 'react-sweetalert2';
 
-export default function Categories() {
+
+function Categories({ swal }) {
+  const [editedCategory, setEditedCategory] = useState(null);
   const [name, setName] = useState("");
   const [categories, setCategories] = useState([]);
   const [parentCategory, setParentCategory] = useState("");
@@ -24,19 +27,66 @@ export default function Categories() {
 
   const saveCategory = async (e) => {
     e.preventDefault();
-    try {
-      const res = await axios.post("/api/categories", { name, parentCategory });
-    } catch (error) {
-      console.log(error.message);
+    const data = { name, parentCategory }
+    if (editedCategory) {
+      try {
+        axios.put(`/api/categories`, {...data, _id: editedCategory._id})
+      } catch (error) {
+        console.log(error.message)
+      }
+      setName("");
+      setParentCategory("")
+      setEditedCategory(null)
+      fetchCategories();
+    } else {
+      try {
+        await axios.post("/api/categories", data);
+      } catch (error) {
+        console.log(error.message);
+      }
+      setName("");
+      setParentCategory("")
+      fetchCategories();
     }
-    setName("");
-    fetchCategories();
+    
   };
+
+  const editCategory = (category) => {
+    setEditedCategory(category);
+    setName(category.name)
+    setParentCategory(category.parent?._id)
+  };
+
+  const deleteCategory = (category) => {
+    swal.fire({
+      title: 'Are you sure?',
+      text: `Do you want delete ${category.name}?`,
+      showCancelButton: true,
+      cancelButtonText: 'Cancel',
+      confirmButtonText: 'Yes, Delete!',
+      confirmButtonColor: 'red',
+      reverseButtons: true,
+    }).then(result => {
+        if (result.isConfirmed) {
+          try {
+            const res = axios.delete(`/api/categories?id=${category._id}`)
+            if (!res) {
+              console.log("Nie ma takiego id")
+            }
+          } catch (error) {
+            console.log(error.message)
+          }
+          fetchCategories() 
+        }
+    })
+  }
 
   return (
     <Layout>
       <h1>Categories</h1>
-      <label htmlFor="cat_name">New category name</label>
+      <label htmlFor="cat_name">
+        {editedCategory ? `Edit category ${editedCategory.name}` : "Create new category"}
+      </label>
       <form className="flex gap-1" onSubmit={saveCategory}>
         <input
           className="mb-0"
@@ -47,14 +97,15 @@ export default function Categories() {
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
-        <select 
-          className="mb-0" 
+        <select
+          className="mb-0"
           value={parentCategory}
-          onChange={(e) => setParentCategory(e.target.value)}>
+          onChange={(e) => setParentCategory(e.target.value)}
+        >
           <option value="">No parent category</option>
           {categories.length > 0 &&
             categories.map((item) => (
-              <option value={item._id}>{item.name}</option>
+              <option value={item._id} key={item._id}>{item.name}</option>
             ))}
         </select>
         <button type="submit" className="btn-primary py-1">
@@ -66,6 +117,7 @@ export default function Categories() {
           <tr>
             <td>Category name</td>
             <td>Parent category</td>
+            <td></td>
           </tr>
         </thead>
         <tbody>
@@ -74,6 +126,21 @@ export default function Categories() {
               <tr key={item.name}>
                 <td>{item.name}</td>
                 <td>{item?.parent?.name}</td>
+                <td>
+                  <div className="flex gap-1 justify-end">
+                    <button
+                      onClick={() => editCategory(item)}
+                      className="btn-primary"
+                    >
+                      Edit
+                    </button>
+                    <button 
+                      onClick={() => deleteCategory(item)}
+                      className="btn-primary">
+                        Delete
+                      </button>
+                  </div>
+                </td>
               </tr>
             ))}
         </tbody>
@@ -81,3 +148,8 @@ export default function Categories() {
     </Layout>
   );
 }
+
+
+export default withSwal(({swal}, ref) => (
+  <Categories swal={swal}/>
+))
